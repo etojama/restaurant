45f42b27dceba479087c8dbb909984a7436b4721
etojama <torohkiss@gmail.com>
2022-01-07 02:03:55 -0800

    added cuisine page with swiper functionality

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   nav.classList.toggle("active");
 });
+
+new Swiper(".swiper", {
+  speed: 400,
+  spaceBetween: 100,
+  effect: "flip",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
